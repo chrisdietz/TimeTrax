0bf5195f86f7e68cd97f46650f3d83c26b3810ff
David Beard <david@david-beard.com>
2014-09-25 23:50:42 -0400

    Added jquery Datepicker, connected Staff and User(Optional), ENum Lists
    Former-commit-id: ce43fd10531723bd9abd9bce9d336fc8db04ebbf

---
+++ TimeTrax/TimeTrax.Web/Models/IdentityModels.cs
@@ -0,0 +1,53 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace TimeTrax.Web.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public virtual Staff Staff { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext() : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Tasks> Tasks { get; set; }
+        public DbSet<TimeEntry> TimeEntries { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            // ASP User
+            modelBuilder.Entity<Staff>()
+                        .HasOptional<ApplicationUser>(staff => staff.User)
+                        .WithOptionalDependent(p => p.Staff);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
