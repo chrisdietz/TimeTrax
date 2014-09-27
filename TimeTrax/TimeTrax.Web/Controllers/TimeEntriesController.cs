f89c317a1ddf356ab5b364ebfb04f5815effc547
David Beard <david@david-beard.com>
2014-09-26 20:01:55 -0400

    Seeding, user info on timesheet
    Former-commit-id: 67d40d376e85b1eba26b160d9dea86979ab2ca77

---
+++ TimeTrax/TimeTrax.Web/Controllers/TimeEntriesController.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TimeTrax.Web.Models;
+
+namespace TimeTrax.Web.Controllers
+{
+    public class TimeEntriesController : Controller
+    {
+        //protected ApplicationDbContext db = new ApplicationDbContext();
+        protected ApplicationDbContext db { get; set; }        
+        protected UserManager<ApplicationUser> UserManager { get; set; }
+
+        public TimeEntriesController()
+        {
+            this.db = new ApplicationDbContext();
+            this.UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(this.db));
+        }
+
+        // GET: TimeEntries
+        public ActionResult Index()
+        {
+            var user = UserManager.FindById(User.Identity.GetUserId());
+            if (user == null)
+            {
+                return RedirectToAction("Login","Account");
+
+            }
+            var timeEntries = db.TimeEntries
+                                .Include(t => t.Project).Include(t => t.Staff).Include(t => t.Tasks)
+                                .Where(t => t.IsActive && t.StaffId == user.Staff.StaffId);
+
+            ViewBag.CurrentUser = user.Staff.FullName;
+            return View(timeEntries.ToList());
+        }
+
+        // GET: TimeEntries/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TimeEntry timeEntry = db.TimeEntries.Find(id);
+            if (timeEntry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(timeEntry);
+        }
+
+        // GET: TimeEntries/Create
+        public ActionResult Create()
+        {
+            var user = UserManager.FindById(User.Identity.GetUserId());
+            ViewBag.ProjectId = new SelectList(db.Projects, "ProjectId", "ProjectName");
+            ViewBag.StaffId = new SelectList(db.Staffs.Where(s => s.IsActive && s.StaffId == user.Staff.StaffId), "StaffId", "FullName", user.Staff.StaffId);
+            ViewBag.TaskId = new SelectList(db.Tasks, "TasksId", "TaskName");
+            return View();
+        }
+
+        // POST: TimeEntries/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TimeEntry timeEntry)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TimeEntries.Add(timeEntry);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            var user = UserManager.FindById(User.Identity.GetUserId());
+            ViewBag.ProjectId = new SelectList(db.Projects, "ProjectId", "ProjectName", timeEntry.ProjectId);
+            ViewBag.StaffId = new SelectList(db.Staffs.Where(s => s.IsActive && s.StaffId == user.Staff.StaffId), "StaffId", "FullName", timeEntry.StaffId);
+            ViewBag.TaskId = new SelectList(db.Tasks, "TasksId", "TaskName", timeEntry.TaskId);
+            return View(timeEntry);
+        }
+
+        // GET: TimeEntries/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TimeEntry timeEntry = db.TimeEntries.Find(id);
+            if (timeEntry == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProjectId = new SelectList(db.Projects, "ProjectId", "ProjectName", timeEntry.ProjectId);
+            ViewBag.StaffId = new SelectList(db.Staffs, "StaffId", "FullName", timeEntry.StaffId);
+            ViewBag.TaskId = new SelectList(db.Tasks, "TasksId", "TaskName", timeEntry.TaskId);
+            return View(timeEntry);
+        }
+
+        // POST: TimeEntries/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TimeEntryId,StaffId,ProjectId,TaskId,Hours,Notes,TimeEntryDate,IsActive,UpdatedDate,UpdatedBy,CreatedDate,CreatedBy")] TimeEntry timeEntry)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(timeEntry).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProjectId = new SelectList(db.Projects, "ProjectId", "ProjectName", timeEntry.ProjectId);
+            ViewBag.StaffId = new SelectList(db.Staffs, "StaffId", "FullName", timeEntry.StaffId);
+            ViewBag.TaskId = new SelectList(db.Tasks, "TasksId", "TaskName", timeEntry.TaskId);
+            return View(timeEntry);
+        }
+
+        // GET: TimeEntries/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TimeEntry timeEntry = db.TimeEntries.Find(id);
+            if (timeEntry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(timeEntry);
+        }
+
+        // POST: TimeEntries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TimeEntry timeEntry = db.TimeEntries.Find(id);
+            db.TimeEntries.Remove(timeEntry);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TimeTrax/TimeTrax.Web/Domain/Initializers/ModelInitializer.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TimeTrax.Web.Models;
+
+namespace TimeTrax.Web.Domain.Initializers
+{
+    public static class ModelInitializer
+    {
+        private static readonly DateTime SeedDate = DateTime.Now;
+        const string SeedBy = "DbInitializer";
+
+        public static List<Project> CreateProjects()
+        {
+            return new List<Project>
+            {
+                new Project
+                {
+                    ProjectId = 1,
+                    ProjectName = "Project Mayhem",
+                    Description = "1st Rule of Project Mayhem: You do not ask questions about Project Mayhem",
+                    IsActive = true,
+                    UpdatedDate = SeedDate,
+                    UpdatedBy = SeedBy,
+                    CreatedDate = SeedDate,
+                    CreatedBy = SeedBy
+                }
+            };
+        }
+                    
+        public static List<Tasks> CreateTasks()
+        {
+            return new List<Tasks>
+            {                
+                new Tasks
+                {
+                    TasksId = 1,
+                    TaskName= "Mischief Committee",
+                    Description = "Setup for Mischief Committee: Meets on Wednesdays",
+                    IsActive = true,
+                    UpdatedDate = SeedDate,
+                    UpdatedBy = SeedBy,
+                    CreatedDate = SeedDate,
+                    CreatedBy = SeedBy
+                },                
+                new Tasks
+                {
+                    TasksId = 2,
+                    TaskName= "Misinformation Committee",
+                    Description = "Setup for Misinformation Committee: Meets on Thursdays or whenever",
+                    IsActive = true,
+                    UpdatedDate = SeedDate,
+                    UpdatedBy = SeedBy,
+                    CreatedDate = SeedDate,
+                    CreatedBy = SeedBy
+                }, 
+            };
+        }
+
+        public static List<Staff> CreateStaffs()
+        {
+            return new List<Staff>
+            {
+                new Staff
+                {
+                    StaffId = 1,
+                    StaffType = "Employee",
+                    FirstName = "Chris",
+                    LastName = "Dietz",
+                    EmailAddress = "chris@technologygrows.com",
+                    DateOfBirth = SeedDate,
+                    IsActive = true,
+                    UpdatedDate = SeedDate,
+                    UpdatedBy = SeedBy,
+                    CreatedDate = SeedDate,
+                    CreatedBy = SeedBy
+                },                                
+                new Staff
+                {
+                    StaffId = 2,
+                    StaffType = "Employee",
+                    FirstName = "David",
+                    LastName = "Beard",
+                    EmailAddress = "david@technologygrows.com",
+                    DateOfBirth = SeedDate,
+                    IsActive = true,
+                    UpdatedDate = SeedDate,
+                    UpdatedBy = SeedBy,
+                    CreatedDate = SeedDate,
+                    CreatedBy = SeedBy
+                },                                
+                new Staff
+                {
+                    StaffId = 3,
+                    StaffType = "Employee",
+                    FirstName = "Nathan",
+                    LastName = "Sinclair",
+                    EmailAddress = "Nathan.Sinclair@FloridaHousing.org",
+                    DateOfBirth = SeedDate,
+                    IsActive = true,
+                    UpdatedDate = SeedDate,
+                    UpdatedBy = SeedBy,
+                    CreatedDate = SeedDate,
+                    CreatedBy = SeedBy
+                },                                
+                new Staff
+                {
+                    StaffId = 4,
+                    StaffType = "Employee",
+                    FirstName = "Blake",
+                    LastName = "Bishop",
+                    EmailAddress = "Blake.Bishop@FloridaHousing.org",
+                    DateOfBirth = SeedDate,
+                    IsActive = true,
+                    UpdatedDate = SeedDate,
+                    UpdatedBy = SeedBy,
+                    CreatedDate = SeedDate,
+                    CreatedBy = SeedBy
+                },                                
+                new Staff
+                {
+                    StaffId = 5,
+                    StaffType = "Employee",
+                    FirstName = "Chester",
+                    LastName = "Taylor",
+                    EmailAddress = "Chester.Taylor@FloridaHousing.org",
+                    DateOfBirth = SeedDate,
+                    IsActive = true,
+                    UpdatedDate = SeedDate,
+                    UpdatedBy = SeedBy,
+                    CreatedDate = SeedDate,
+                    CreatedBy = SeedBy
+                },                                
+                new Staff
+                {
+                    StaffId = 6,
+                    StaffType = "Employee",
+                    FirstName = "Roberto",
+                    LastName = "Cuba",
+                    EmailAddress = "cuba.rocha.roberto@gmail.com",
+                    DateOfBirth = SeedDate,
+                    IsActive = true,
+                    UpdatedDate = SeedDate,
+                    UpdatedBy = SeedBy,
+                    CreatedDate = SeedDate,
+                    CreatedBy = SeedBy
+                },                                
+                new Staff
+                {
+                    StaffId = 7,
+                    StaffType = "Employee",
+                    FirstName = "Curtis",
+                    LastName = "Johnson",
+                    EmailAddress = "crjohnson3@fsu.edu",
+                    DateOfBirth = SeedDate,
+                    IsActive = true,
+                    UpdatedDate = SeedDate,
+                    UpdatedBy = SeedBy,
+                    CreatedDate = SeedDate,
+                    CreatedBy = SeedBy
+                },                                
+                new Staff
+                {
+                    StaffId = 8,
+                    StaffType = "Employee",
+                    FirstName = "Brandon",
+                    LastName = "Iglesias",
+                    EmailAddress = "iglesias.brandon@gmail.com",
+                    DateOfBirth = SeedDate,
+                    IsActive = true,
+                    UpdatedDate = SeedDate,
+                    UpdatedBy = SeedBy,
+                    CreatedDate = SeedDate,
+                    CreatedBy = SeedBy
+                },                                
+                new Staff
+                {
+                    StaffId = 9,
+                    StaffType = "Employee",
+                    FirstName = "Michael",
+                    LastName = "Iglesias",
+                    EmailAddress = "michael@technologygrows.com",
+                    DateOfBirth = SeedDate,
+                    IsActive = true,
+                    UpdatedDate = SeedDate,
+                    UpdatedBy = SeedBy,
+                    CreatedDate = SeedDate,
+                    CreatedBy = SeedBy
+                },                                
+                new Staff
+                {
+                    StaffId = 10,
+                    StaffType = "Employee",
+                    FirstName = "Ryan",
+                    LastName = "Whitney",
+                    EmailAddress = "ryan@technologygrows.com",
+                    DateOfBirth = SeedDate,
+                    IsActive = true,
+                    UpdatedDate = SeedDate,
+                    UpdatedBy = SeedBy,
+                    CreatedDate = SeedDate,
+                    CreatedBy = SeedBy
+                },                                
+                new Staff
+                {
+                    StaffId = 11,
+                    StaffType = "Employee",
+                    FirstName = "Thomas",
+                    LastName = "Jackson",
+                    EmailAddress = "thomas@technologygrows.com",
+                    DateOfBirth = SeedDate,
+                    IsActive = true,
+                    UpdatedDate = SeedDate,
+                    UpdatedBy = SeedBy,
+                    CreatedDate = SeedDate,
+                    CreatedBy = SeedBy
+                },                                
+                new Staff
+                {
+                    StaffId = 12,
+                    StaffType = "Employee",
+                    FirstName = "Lucas",
+                    LastName = "vonHollan",
+                    EmailAddress = "lucas@technologygrows.com",
+                    DateOfBirth = SeedDate,
+                    IsActive = true,
+                    UpdatedDate = SeedDate,
+                    UpdatedBy = SeedBy,
+                    CreatedDate = SeedDate,
+                    CreatedBy = SeedBy
+                }
+            };
+        }
+        
+    }
+}
