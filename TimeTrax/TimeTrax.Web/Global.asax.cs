6e63e5401f6cddfd5bc9b23bfca77ec791ec391a
chrisdietz <chris@selenko.com>
2014-09-29 14:58:02 -0400

    changed Web Config to local
    Former-commit-id: 65d43e185f5cb0d617de3948b47d5d58ad44b199

---
+++ TimeTrax/TimeTrax.Web/Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using TimeTrax.Web.Data.Commands;
+using TimeTrax.Web.Models;
+
+namespace TimeTrax.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            // To initialize the database, RUN ONLY ONE TIME!!!
+            //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<ApplicationDbContext>());
+            //var db = new ApplicationDbContext();
+            //var seed = new SeedDatabaseCommand(db);
+            //seed.SeedDatabase();
+            
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ TimeTrax/TimeTrax.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TimeTrax.Web.Startup))]
+namespace TimeTrax.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
